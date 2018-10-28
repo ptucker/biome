99638d0635c4965c2fd91d2cde7c9571831cc1c7
ptucker <ptucker@whitworth.edu>
2018-10-28 12:18:43 -0700

    Use relative paths for images, rather than hard-coded paths

--- Cell Matching/Assets/PlayCellMatchGame.cs
+++ Cell Matching/Assets/PlayCellMatchGame.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayCellMatchGame : MonoBehaviour {
 
-	string jsonFile = @"..\GameData\celldrec.json";
+	string jsonFile = @"..\GameData\cellrec.json";
 
 	List<OrganelleLabel> labels;
 	OrganelleLabel dragTarget;
